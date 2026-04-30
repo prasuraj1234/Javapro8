34d9d3f5ab93c17ab03c073cf58cc97cbf4fbc8f
Suraj Prakash <110290442+prasuraj1234@users.noreply.github.com>
2026-04-30 19:10:06 +0530

    Update Phone.java

--- Phone.java
+++ Phone.java
@@ -1,1 +1,21 @@
+package Interface;
 
+interface Samsung {
+    public void Mobile();
+
+    public default void htc() {
+        System.out.println("Htc is the also a good phone");
+    }
+}
+
+public class Phone implements Samsung {
+    public void Mobile() {
+        System.out.println("Samsung is the good mobile");
+    }
+
+    public static void main(String[] args) {
+        Phone p = new Phone();
+        p.Mobile();
+        p.htc();
+    }
+}
