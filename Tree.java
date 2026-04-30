fe084d730ca69e34dfda74c8ea068cfb24604f62
Suraj Prakash <110290442+prasuraj1234@users.noreply.github.com>
2026-04-30 19:13:01 +0530

    Update Tree.java

--- Tree.java
+++ Tree.java
@@ -1,1 +1,36 @@
 
+package constructor;
+
+public class Fruit {
+	String name;
+	double weight;
+	double price;
+		Fruit(String name)
+		{
+			this.name=name;
+		}
+		Fruit(String name, double weight)
+		{
+			this.name=name;
+			this.weight=weight;
+		}
+		Fruit(String name, double weight, double price)
+		{
+			this.name=name;
+			this.price=price;
+			this.weight=weight;
+		}
+		public void displayFruit()
+		{
+			System.out.println("name of fruit:"+name);
+			System.out.println("weight of fruit:"+weight);
+			System.out.println("price of fruit:"+price );
+		}
+	
+	
+		
+		
+	
+	
+
+}
